e338199b393b0d2dbef13078fa13cccb1df42a86
jeexan2 <jeexan2@gmail.com>
2024-07-20 02:12:09 +0700

    Initial Data Storage Design

---
+++ OldPhoneApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+
+
+class Program{
+
+    static void Main(string[] args){
+        // Dictionary for key to char mapper
+        Dictionary<String,String> keyNumStringMap = new Dictionary<String,String>();
+
+        IList<String> buttons = new List<String>(){
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "*",
+            "#"
+        };
+
+        String endCharacterCursor = "#";
+
+        IList<String> characterList = new List<String>(){
+            "&'(",
+            "abcABC",
+            "defDEF",
+            "ghiGHI",
+            "jklJKL",
+            "mnoMNO",
+            "pqrsPQRS",
+            "tuvTUV",
+            "wxyzWXYZ",
+            "|x|",
+            "|=>|"
+        };
+
+
+        //TODO: Input validation between two list
+        for(int i = 0; i < buttons.Count;i++){
+            keyNumStringMap.Add(buttons[i],characterList[i]);
+        }
+        
+
+
+    }
+}
